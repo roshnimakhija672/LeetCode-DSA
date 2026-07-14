31dc107d985a3cf53a2d1da67922f7a1a934ffb6
Roshni Makhija <roshnimakhija672@gmail.com>
2026-07-14 23:00:13 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (83.06%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Solution {
+
+    // Function to find the insert position of x in sorted array
+    public int searchInsert(int[] arr, int x) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int ans = n; // Default to end if x is greater than all elements
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] >= x) {
+                // Potential answer found, try to go left
+                ans = mid;
+                high = mid - 1;
+            } else {
+                // Go right
+                low = mid + 1;
+            }
+        }
+    
+        return ans;
+}
+    
+}
