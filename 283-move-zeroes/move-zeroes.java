ea704d019324720e8081b48c9965764af66260a0
Roshni Makhija <roshnimakhija672@gmail.com>
2026-07-22 20:49:35 +0530

    Time: 2 ms (92.14%) | Memory: 47.6 MB (88.48%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    // Function to move zeroes to the end
+    public void moveZeroes(int[] nums) {
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zero found, return
+        if (j == -1) return;
+
+        // Start from the next index of first zero
+        for (int i = j + 1; i < nums.length; i++) {
+            // If current element is non-zero
+            if (nums[i] != 0) {
+                // Swap with nums[j]
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                // Move j to next zero
+                j++;
+            }
+        }
+    }
+}
